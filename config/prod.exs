4aeb44f4bf27202ffc4c0863110033d0e0a8e300
Tony Miller <mcfiredrill@gmail.com>
2015-08-22 14:02:23 -0700

    change host for prod

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # meaningful, we use this information when generating URLs.
 config :chat, Chat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"]
+  url: [host: "hotdog-lounge.herokuapp.com"]
 
 # ## SSL Support
 #
