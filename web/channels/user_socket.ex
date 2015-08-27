ce1b7bb61cd12c67cf2f0de3c0411cf39e973a29
Tony Miller <mcfiredrill@gmail.com>
2015-08-27 11:07:57 -0700

    change allowed origins

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -4,9 +4,9 @@ defmodule Chat.UserSocket do
   channel "rooms:*", Chat.RoomChannel
 
   transport :websocket, Phoenix.Transports.WebSocket,
-    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:9000", "//datafruits.fm"]
+    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:4200", "//datafruits.fm"]
   transport :longpoll, Phoenix.Transports.LongPoll,
-    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:9000", "//datafruits.fm"]
+    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:4200", "//datafruits.fm"]
 
   def connect(_params, socket) do
     {:ok, socket}
