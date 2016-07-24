82100109f27761ac3856761862a058eb128ceffd
Tony Miller <mcfiredrill@gmail.com>
2016-07-24 15:32:59 +0900

    test new:msg

--- test/channels/room_channel_test.exs
+++ test/channels/room_channel_test.exs
@@ -11,15 +11,29 @@ defmodule Chat.RoomChannelTest do
     {:ok, socket: socket}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
+  # test "ping replies with status ok", %{socket: socket} do
+  #   ref = push socket, "ping", %{"hello" => "there"}
+  #   assert_reply ref, :ok, %{"hello" => "there"}
+  # end
+  #
+  # test "shout broadcasts to rooms:lobby", %{socket: socket} do
+  #   push socket, "shout", %{"hello" => "all"}
+  #   assert_broadcast "shout", %{"hello" => "all"}
+  # end
+
+  test "broadcasts new messages", %{socket: socket} do
+    message = %{"user" => "ovenrake", "body" => "i love :hotdogs:", "timestamp" => 201623523}
+    ref = push socket, "new:msg", message
+    assert_broadcast "new:msg", message
+    body = message["body"]
+    assert_reply ref, :ok, %{msg: body}
   end
 
-  test "shout broadcasts to rooms:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
+  test "handles authorize"
+
+  test "authorization fails if nick is too long"
+
+  test "authorization fails if nick is taken"
 
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from! socket, "broadcast", %{"some" => "data"}
--- test/chat_test.exs
+++ test/chat_test.exs
@@ -1,7 +1,4 @@
 defmodule ChatTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
