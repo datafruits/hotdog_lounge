e6a365d3e11626e285a0cc089df048771cd61b97
Tony Miller <mcfiredrill@gmail.com>
2017-09-12 13:06:46 +0900

    use phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Chat.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.1.2"},
+    [{:phoenix, "~> 1.3"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:phoenix_ecto, "~> 2.0"},
