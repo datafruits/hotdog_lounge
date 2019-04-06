05d713c4aaca3fc8235d384f31153e8a47787dc2
Tony Miller <mcfiredrill@gmail.com>
2019-04-06 17:01:01 +0900

    check more origins

--- lib/chat/endpoint.ex
+++ lib/chat/endpoint.ex
@@ -4,7 +4,7 @@ defmodule Chat.Endpoint do
   socket "/socket", Chat.UserSocket,
     websocket: true,
     longpoll: true,
-    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:4200", "//localhost:3000", "//datafruits.fm", "//datafruits-fastboot.herokuapp.com/", "//www.datafruits.fm"]
+    check_origin: ["//datafruitstest.s3-website-us-east-1.amazonaws.com/", "//localhost:4200", "//localhost:3000", "//datafruits.fm", "//datafruits-fastboot.herokuapp.com/", "//www.datafruits.fm", "https://datafruits.fm", "https://www.datafruits.fm"]
 
   # Serve at "/" the given assets from "priv/static" directory
   # don't need this static stuff
