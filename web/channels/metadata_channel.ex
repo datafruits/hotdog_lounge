a347b00826064232b35cefe7365045f6e316926e
Tony Miller <mcfiredrill@gmail.com>
2019-10-22 22:37:52 +0900

    specify redis host in metadata_channel

--- web/channels/metadata_channel.ex
+++ web/channels/metadata_channel.ex
@@ -3,11 +3,11 @@ defmodule Chat.MetadataChannel do
   require Logger
 
   def join("metadata", message, socket) do
-    {:ok, pubsub} = Redix.PubSub.start_link(password: System.get_env("REDIS_PASSWORD"))
+    {:ok, pubsub} = Redix.PubSub.start_link(host: System.get_env("REDIS_HOST"), password: System.get_env("REDIS_PASSWORD"))
 
     Redix.PubSub.subscribe(pubsub, "datafruits:metadata", self())
 
-    {:ok, conn} = Redix.start_link(password: System.get_env("REDIS_PASSWORD"))
+    {:ok, conn} = Redix.start_link(host: System.get_env("REDIS_HOST"), password: System.get_env("REDIS_PASSWORD"))
     {:ok, message} = Redix.command(conn, ["GET", "datafruits:metadata"])
 
     send(self, {:after_join, message})
