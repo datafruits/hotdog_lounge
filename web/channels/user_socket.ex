6745ee353f0e3529b5a33eb33f5c214adf78cfe7
Tony Miller <mcfiredrill@gmail.com>
2020-02-24 12:33:05 +0900

    wip ban by IP address

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,13 +1,23 @@
 defmodule Chat.UserSocket do
   use Phoenix.Socket
+  require Logger
 
   channel "rooms:*", Chat.RoomChannel
   channel "metadata", Chat.MetadataChannel
   channel "notifications", Chat.NotificationChannel
 
-  def connect(_params, socket) do
+  def connect(_params, socket, connect_info) do
+    remote_ip = connect_info.peer_data.address
+    # need to store IP somewhere?
+    #
+    # dont return OK if banned...
+    # if remote_ip in banned_list
+    # { :banned, socket }
+    # end
     {:ok, socket}
   end
 
-  def id(_socket), do: nil
+  def id(socket) do
+    "user_socket:#{socket.assigns.remote_ip}"
+  end
 end
