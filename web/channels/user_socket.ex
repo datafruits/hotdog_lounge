bba6a0ee6654c472f06284d2af85fee36c886284
Tony Miller <mcfiredrill@gmail.com>
2020-03-22 13:36:38 +0900

    parse remote IP

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -7,17 +7,20 @@ defmodule Chat.UserSocket do
   channel "notifications", Chat.NotificationChannel
 
   def connect(_params, socket, connect_info) do
-    remote_ip = connect_info.peer_data.address
+    # remote_ip = connect_info.peer_data.address
+    remote_ip = Enum.join(Tuple.to_list(connect_info.peer_data.address), ".")
+    Logger.debug "remote ip: #{remote_ip}"
     # need to store IP somewhere?
     #
     # dont return OK if banned...
     # if remote_ip in banned_list
     # { :banned, socket }
     # end
-    {:ok, socket}
+    {:ok, assign(socket, :remote_ip, remote_ip)}
   end
 
   def id(socket) do
-    "user_socket:#{socket.assigns.remote_ip}"
+    remote_ip = socket.assigns.remote_ip
+    "user_socket:#{remote_ip}"
   end
 end
